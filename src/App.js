4cb70ef4822cfd9be09edb2790937492ee507625
dev1980 <brham1980@gmail.com>
2020-05-03 21:42:46 +0545

    fixed linter error

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
         <CategoryFilter 
           changeFilter={changeFilter} 
           filter={filter} 
-          categories={categories} />
+        categories={categories} />
         <Recipes recipes={filterRecipe} />
       </div>
     );
