5cd7fef7d8429468b884ea5c82df1111dacb29d5
dev1980 <brham1980@gmail.com>
2020-05-03 18:38:10 +0545

    fixed linter error

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,22 +6,21 @@ import { Link } from 'react-router-dom';
 const Recipes = ({ recipes }) => (
   <div className="container">
     <div className="row">
-      {recipes.map(res => {  
-        return (
-          <div key={res.idMeal} className="col-md-4" style={{ marginBottom:'2rem' }}>
-          <div className="recipes__box">
+      {recipes.map(res => { return (
+          <div key={res.idMeal} className="col-md-4" style={{ marginBottom: '2rem' }}>
+            <div className="recipes__box">
             <img className="recipe__box-img" src={res.strMealThumb} alt={res.strMeal} />
              <div className="recipe__text">
-                <h5 className="recipes__title">
+               <h5 className="recipes__title">
                   {res.strMeal.length < 20 ? `${res.strMeal}` : `${res.strMeal.substring(0, 25)}...`}
                 </h5>
-                <p className="recipes__subtitle">
+               <p className="recipes__subtitle">
                 Publisher: 
-                <span>{res.strMeal }</span>
+                  <span>{res.strMeal }</span>
                 </p>
              </div>
             <button className="recipe_buttons" type="button">
-              <Link to={{ pathname: `/recipe/${res.idMeal}`, state: { recipe: res.strMeal }}}>View Recipe</Link>
+              <Link to={ { pathname: `/recipe/${res.idMeal}`, state: { recipe: res.strMeal }} }>View Recipe</Link>
             </button>
           </div>
           </div>
