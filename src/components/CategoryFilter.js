6fec063414135b664fa77713b60a08128eacd2c3
dev1980 <brham1980@gmail.com>
2020-05-03 11:47:01 +0545

    add category filter

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+
+const CategoryFilter = ({ changeFilter, categories, filter }) => (
+  <select onChange={e => changeFilter(e.target.value)} className="header-select mb-4" value={filter}>
+  <option value= 'All Categories'>All Categories</option>
+    {categories.map(category => (
+      <option key={`${category.strCategory}-option`} value={category.strCategory}>
+        {category.strCategory}
+      </option>
+    ))}
+  </select>
+);
+
+CategoryFilter.propTypes = {
+  changeFilter: Proptypes.func.isRequired,
+  filter: Proptypes.string.isRequired,
+  categories: Proptypes.instanceOf(Array).isRequired,
+};
+
+export default CategoryFilter;
