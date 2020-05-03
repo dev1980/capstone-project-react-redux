5c84ed53cd7d8af7640198f6db8b9f5648aa06a4
dev1980 <brham1980@gmail.com>
2020-05-03 18:43:56 +0545

    fixed linter error

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -12,19 +12,19 @@ const Recipes = ({ recipes }) => (
             <img className="recipe__box-img" src={res.strMealThumb} alt={res.strMeal} />
              <div className="recipe__text">
                <h5 className="recipes__title">
-                  {res.strMeal.length < 20 ? `${res.strMeal}` : `${res.strMeal.substring(0, 25)}...`}
+                 {res.strMeal.length < 20 ? `${res.strMeal}` : `${res.strMeal.substring(0, 25)}...`}
                 </h5>
                <p className="recipes__subtitle">
                 Publisher: 
-                  <span>{res.strMeal }</span>
-                </p>
+                 <span>{res.strMeal }</span>
+               </p>
              </div>
-            <button className="recipe_buttons" type="button">
-              <Link to={ { pathname: `/recipe/${res.idMeal}`, state: { recipe: res.strMeal }} }>View Recipe</Link>
+              <button className="recipe_buttons" type="button">
+              <Link to={{pathname: `/recipe/${res.idMeal}`, state: {recipe: res.strMeal}}}>View Recipe</Link>
             </button>
+            </div>
           </div>
-          </div>
-        );
+      );
       })}
     </div>
   </div>
