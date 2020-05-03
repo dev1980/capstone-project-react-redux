053416d2d3b13eef8a7be8c4b12028dbe06ce961
dev1980 <brham1980@gmail.com>
2020-05-03 18:57:48 +0545

    fixed linter error

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -8,7 +8,7 @@ const Recipes = ({ recipes }) => (
     <div className="row">
       {recipes.map(res => 
         { return (
-        <div key={res.idMeal} className="col-md-4" style={{ marginBottom: '2rem' }}>
+          <div key={res.idMeal} className="col-md-4" style={{ marginBottom: '2rem' }}>
           <div className="recipes__box">
               <img className="recipe__box-img" src={res.strMealThumb} alt={res.strMeal} />
               <div className="recipe__text">
@@ -17,13 +17,13 @@ const Recipes = ({ recipes }) => (
                </h5>
                 <p className="recipes__subtitle">
                 Publisher: 
-                 <span>{res.strMeal }</span>
-               </p>
+                  <span>{res.strMeal }</span>
+                </p>
               </div>
-              <button className="recipe_buttons" type="button">
-                <Link to={{ pathname: `/recipe/${res.idMeal}`, state: {recipe: res.strMeal} }}>View Recipe</Link>
-              </button>
-            </div>
+            <button className="recipe_buttons" type="button">
+              <Link to={ {pathname: `/recipe/${ res.idMeal }`, state: {recipe: res.strMeal}} }>View Recipe</Link>
+            </button>
+          </div>
         </div>
       );
       })}
